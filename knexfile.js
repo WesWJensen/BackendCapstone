065986e1ba915aea4eb49a4e520c13792fd230ea
Wesley Jensen <wesjensenmusic@gmail.com>
2022-02-04 14:13:26 -0500

    post seed run

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://alejxvdc:TcfwAldwX_GsNiJBRNwCRXpNcrMwt08X@castor.db.elephantsql.com/alejxvdc",
 } = process.env;
 
 module.exports = {
